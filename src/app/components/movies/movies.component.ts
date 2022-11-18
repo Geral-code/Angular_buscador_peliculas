9c2199ff7dc4bee3e04ad4aea19d9c2509f48860
Geraldine <geraldine.orellana@gmail.com>
2022-11-17 23:53:57 -0300

    agrega operadores

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,5 +1,7 @@
 import { ThisReceiver } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, distinct, filter, fromEvent, map, Observable, Subscription, switchMap, tap } from 'rxjs';
+import { Movie } from 'src/app/interfaces/movies';
 import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
@@ -8,17 +10,34 @@ import { MovieService } from 'src/app/services/movie.service';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
-  movies: any[] = [];
+  movies: Movie[] = [];
+  @ViewChild('movieSearchInput', { static: true }) movieSearchInput!: ElementRef
+  movies$!: Observable<Movie[]>
 
   constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.movies$ = fromEvent<Event>(this.movieSearchInput.nativeElement, 'keyup').pipe(  //escuchando los caracteres que el usuario esta escribiendo
+      map((event: Event) => {                                            // estamos mapeando ese caracter para obtener el termino
+        const searchTerm = (event.target as HTMLInputElement).value
+        return searchTerm 
+      }),
+      filter((searchTerm: string) => searchTerm.length > 3), // OPERADOR-cuando llegue al 4 caracter hace la peticion HTTP
+      debounceTime(500),                                    // OPERADOR-cuando pare de escribir por medio seg. se hara la peticion HTTP 
+      distinct(),                                           // OPERADOR-cuando se escribe lo mismo que lo anterior no se hace otra peticion si no la misma
+      switchMap((searchTerm: string) =>  this.movieService.getMovies(searchTerm)) 
+    ) 
+       //
+    
   }
 
-  getMovies(searchTerm: string) {
-   this.movieService.getMovies(searchTerm).subscribe(movies => {
+
+  
+
+/*  getMovies(searchTerm: string) {                       // Y ejecuta la peticion HTTP
+    this.movieService.getMovies(searchTerm).subscribe(movies => { 
     console.log(movies);
     this.movies = movies !== undefined ? movies : [];
    })
-  }
+  } */
 }
